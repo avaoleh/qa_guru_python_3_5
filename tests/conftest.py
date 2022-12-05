2f6f6c15683ae4ccf7d1385458cd774b9ad6d020
AvaOleh <avakimyanov@gmail.com>
2022-12-05 17:47:59 +0300

    added conftest.py and test_demoqa_3_5.py

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selene import have, command
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_management():
+    browser.config.window_width = 1080
+    browser.config.window_height = 950
+    browser.config.hold_browser_open = True
+    browser.open('https://demoqa.com/automation-practice-form')
+    ads = browser.all('[id^=google_ads_][id$=container__]')
+    ads.should(have.size_less_than_or_equal(3))
+    ads.perform(command.js.remove)
+    yield
+    browser.close()
