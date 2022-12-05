3ea5afbd5857258e1745cb5e8a9541e3400c64e4
AvaOleh <avakimyanov@gmail.com>
2022-12-05 18:51:15 +0300

    added some changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,15 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_management():
-    browser.config.window_width = 1080
-    browser.config.window_height = 950
+    #browser.config.window_width = 1080
+    #browser.config.window_height = 950
     browser.config.hold_browser_open = True
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.open('https://demoqa.com/automation-practice-form').driver.maximize_window()
+
+    #remove/close all advertisements:
     ads = browser.all('[id^=google_ads_][id$=container__]')
     ads.should(have.size_less_than_or_equal(3))
     ads.perform(command.js.remove)
+
     yield
     browser.close()
---
+++ tests/test_demoqa_3_5.py
@@ -0,0 +1,33 @@
+from selene.support.shared import browser
+from selene import be, have
+import os
+
+def test_demoqa_3_5():
+    browser.element('#firstName').type('Oleg')
+    browser.element('#lastName').type('Avakimyanov')
+    browser.element('#userEmail').type('testqa@gmail.com')
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type('1234567890')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').type("July")
+    browser.element('.react-datepicker__year-select').type("2000")
+    browser.element('[aria-label="Choose Tuesday, July 25th, 2000"]').click()
+    browser.element('#subjectsInput').type('Computer Science').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture').set_value(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'files/tools_qa.png')))
+    browser.element('#currentAddress').type('Antares')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('Delhi').press_enter()
+    browser.element('#submit').press_enter()
+
+    browser.all('.table-responsive td:nth-child(2)').should(have.texts(
+        'Oleg Avakimyanov',
+        'testqa@gmail.com',
+        'Male',
+        '1234567890',
+        '25 July,2000',
+        'Computer Science',
+        'Sports',
+        'tools_qa.png',
+        'Antares',
+        'NCR Delhi'))
